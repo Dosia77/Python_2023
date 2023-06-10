a8b19549da1a2afe341ffd24d65bc218e139ba11
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-06-11 02:36:18 +0300

    added seminar_7

--- seminar_7.py
+++ seminar_7.py
@@ -1,3 +1,7 @@
+print()
+print('Задача про Винни-Пуха')
+print()
+
 c = []
 a = (input('введите сопелку  ')).split()
 
@@ -5,4 +9,17 @@ for i in range(len(a)):
     c.append(len(list(filter(lambda x: x in 'аоеиёыуэюя', a[i]))))
    
 print((lambda x: 'Парам пам-пам' if len(set(x)) == 1 else 'Пам парам')(c))
+
+
+print()
+print('Функция print_operation_table')
+print()
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    arr = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in arr:
+        print(*[f"{x:>3}" for x in i])
+
+print_operation_table(lambda x, y: x * y)
+
 
